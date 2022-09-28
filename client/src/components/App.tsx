a4f85a7c1834ee811e93cfd04f9d3c30c4935677
NkemdiAnyiam <NkemdiAnyiam@gmail.com>
2022-09-27 22:07:09 -0500

    Set up basic routing

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Router, Route, Switch } from "react-router-dom";
+import history from "../history";
+
 import '../styles/main.scss';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">App</div>
+      <div className="App">
+        <Router history={history}>
+          <Switch>
+            <Route exact path="/">
+              <div>App</div>
+            </Route>
+            <Route path="*" status={404}>
+              404
+            </Route>
+          </Switch>
+        </Router>
+      </div>
     );
   }
 }
