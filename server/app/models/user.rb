46e6f17af80c093abf5af4961d6c81a6db49ec39
Niyi Ajobiewe <adeniyi.ajobiewe@gmail.com>
2022-10-07 15:07:36 -0500

    added test

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   validates :username, presence: true
   validates :username, uniqueness: true
   validates :username, length: { minimum: 4 }
+  # validates :password_digest, presence: true
 end
--- server/spec/models/user_spec.rb
+++ server/spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { User.new(username: "neyobrainy", email: "jsmith@sample.com", password_digest: "ligali" )}
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+  it "is not valid without a username" do
+    subject.username=nil
+    expect(subject).to_not be_valid
+  end
+  it "is not valid without a password_digest" do
+    subject.password_digest=nil
+    expect(subject).to_not be_valid
+  end
 end
