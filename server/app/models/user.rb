c00098343166691916e2a3142549440f9cda46b5
ganan-tamu <ganan@tamu.edu>
2022-10-07 00:53:01 -0500

    validates password and usertype

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :password, length: {minimum: 8}
   validates :username, presence: true
   validates :username, uniqueness: true
   validates :username, length: { minimum: 4 }
+  validates :usertype,
+    :inclusion  => { :in => [ 'company representative', 'student', 'faculty', 'admin', 'volunteer'],
+    :message    => "%{value} is not a valid usertype" }
 end
