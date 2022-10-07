98be8309e267520644341f7f66dc978448a5761b
ganan-tamu <ganan@tamu.edu>
2022-10-07 06:58:09 -0500

    created a company rep sign up page that will save the info users enter into the database

--- server/app/controllers/users_controller.rb
+++ server/app/controllers/users_controller.rb
@@ -48,6 +48,10 @@ class UsersController < ApplicationController
     end
   end
 
+  def new
+    @user = User.new
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   get '/logged_in', to: 'sessions#is_logged_in?'
   
   get '/users/find', to: 'users#show_by_find'
+
+  get '/company-representative-signup', to: 'users#new'
+  post '/company-representative-signup', to: 'users#create'
+
   resources :users, only: [:create, :show, :index] do
     resources :items, only: [:create, :show, :index, :destroy]
   end
