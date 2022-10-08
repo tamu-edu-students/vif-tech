bb1373f154f4cbe799ee562d380b281730127050
NkemdiAnyiam <NkemdiAnyiam@gmail.com>
2022-10-08 17:23:01 -0500

    Move some configuration options into axios instance

--- client/src/apis/vifTech.ts
+++ client/src/apis/vifTech.ts
@@ -2,5 +2,7 @@ import axios from "axios";
 console.log('BASE URL:', process.env.REACT_APP_BASE_URL)
 
 export default axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL
+  baseURL: process.env.REACT_APP_BASE_URL,
+  withCredentials: true,
+  headers: { 'Content-Type': 'application/json' }
 });
