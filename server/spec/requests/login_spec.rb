8f5ede8f383d1344a46070fd93f5168159dc3626
Niyi Ajobiewe <adeniyi.ajobiewe@gmail.com>
2022-10-10 14:52:52 -0500

    added payload tests

--- server/spec/requests/login_spec.rb
+++ server/spec/requests/login_spec.rb
@@ -12,10 +12,27 @@ RSpec.describe 'Login', type: :request do
           expect(json['user']).not_to eq(nil)
           expect(json['logged_in']).to eq(true)
         end
+
         it 'returns status code 200' do
           expect(response).to have_http_status(200)
         end
       end
-    end
 
-   end
+      context 'Invalid Login Payload Returns Error' do
+        
+        it 'User cannot login with wrong username' do
+          login_payload[:user][:username] = 'wrongusername';
+          post '/login', params: login_payload
+          json = JSON.parse(response.body)
+          expect(json['status']).to eq(401)
+        end
+
+        it 'User cannot login with wrong password' do
+          login_payload[:user][:password] = 'wrong';
+          post '/login', params: login_payload
+          json = JSON.parse(response.body)
+          expect(json['status']).to eq(401)
+        end
+      end
+    end
+end
