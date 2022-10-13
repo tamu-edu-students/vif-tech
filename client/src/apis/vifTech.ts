8a24edcd54e403d3c43c3da23608043b91eb44dc
NkemdiAnyiam <NkemdiAnyiam@gmail.com>
2022-10-12 19:14:36 -0500

    Throw error if REACT_APP_BASE_URL is not defined

--- client/src/apis/vifTech.ts
+++ client/src/apis/vifTech.ts
@@ -1,5 +1,8 @@
 import axios from "axios";
-console.log('BASE URL:', process.env.REACT_APP_BASE_URL)
+console.log('BASE URL:', process.env.REACT_APP_BASE_URL);
+if (!process.env.REACT_APP_BASE_URL) {
+  throw new Error('REACT_APP_BASE_URL environment variable not found. API calls will not work unless set.');
+}
 
 export default axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
