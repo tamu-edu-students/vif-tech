fa397914341e98f65ed8d389c96e3d7e4aacde39
Insoo Chung <insoooo.chung@gmail.com>
2022-10-20 15:29:45 -0500

    Set previleges for usermeeting routes

--- server/app/controllers/user_meetings_controller.rb
+++ server/app/controllers/user_meetings_controller.rb
@@ -10,13 +10,26 @@ class UserMeetingsController < ApplicationController
     end
   end
 
+  def confirm_requester_is_admin
+    if current_user.usertype != "admin"
+      render json: {
+        status: 500,
+        errors: ["User does not have previleges for requested action"],
+      }
+      return false
+    end
+    return true
+  end
+
   def index
+    confirm_requester_is_admin
     render json: {
              user_meetings: UserMeeting.all,
            }
   end
 
   def show
+    confirm_requester_is_admin
     @user_meeting = UserMeeting.find_by_id(params[:id])
     if @user_meeting
       render json: {
