192f4f61721219a8fba6990f2fe5452bc7682d4d
ganan-tamu <ganan@tamu.edu>
2022-10-20 21:12:51 -0500

    added usertype default in schema.rb

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -23,11 +23,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_19_162136) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "usertype"
     t.boolean "email_confirmed", default: false
     t.string "confirm_token"
     t.string "firstname"
     t.string "lastname"
+    t.string "usertype", default: "student"
   end
 
 end
