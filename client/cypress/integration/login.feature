8f0ee92c1008bc1080b9eb9be7d9358ce120a907
NkemdiAnyiam <NkemdiAnyiam@gmail.com>
2022-10-21 18:14:05 -0500

    Add more testing and UX correctness for login/registration

--- client/cypress/integration/login.feature
+++ client/cypress/integration/login.feature
@@ -3,6 +3,35 @@ Feature: Login
   Background: starting from home page
     Given I am on the home page
 
+  Scenario: log in button visible and log out button invisible when not logged in
+    Given login session is inactive
+    And login status is checked
+    Then the log in page button should be visible
+    And the sign up page button should be visible
+    And the log out button should not be visible
+
+  Scenario: login button redirects to login page
+    Given login session is inactive
+    And login status is checked
+    When I click the log in page button
+    Then I should be redirected to the login page
+
+  Scenario Outline: logout button redirects to home page
+    Given login session is active with the following email:
+      | email   |
+      | <email> |
+    And login status is checked
+    When I click the log out button
+    Then I should be redirected to the home page
+    Then login status is checked
+    Then I should not see my first name and last name in the nav bar
+      | firstname   | lastname   | 
+      | <firstname> | <lastname> |
+
+    Examples:
+    | email               | password | firstname | lastname |
+    | usedEmail@gmail.com | abcdefg  | Oldboy    | Senior   |
+
   Scenario Outline: valid login credentials
     Given login session is inactive
     And login status is checked
@@ -10,7 +39,7 @@ Feature: Login
     And I provide the following:
       | email   | password  | 
       | <email> |<password> |
-    And I click the log in button
+    And I click the log in form button
     Then a session should start with the following email:
       | email   |
       | <email> |
@@ -40,6 +69,19 @@ Feature: Login
     | email               | password | firstname | lastname |
     | usedEmail@gmail.com | abcdefg  | Oldboy    | Senior   |
 
+  Scenario Outline: log out button should be visible and log in button should not be visible when logged in
+    Given login session is active with the following email:
+      | email   |
+      | <email> |
+    And login status is checked
+    Then the log out button should be visible
+    And the log in button should not be visible
+    And the sign up page button should not be visible
+
+    Examples:
+    | email               | password | firstname | lastname |
+    | usedEmail@gmail.com | abcdefg  | Oldboy    | Senior   |
+
   Scenario Outline: invalid login credentials
     Given login session is inactive
     And login status is checked
@@ -47,7 +89,7 @@ Feature: Login
     When I provide the following:
       | email   | password   |
       | <email> | <password> |
-    And I click the log in button
+    And I click the log in form button
     Then I should see an error
     And I should remain on the login page
 
--- client/cypress/integration/login/login.js
+++ client/cypress/integration/login/login.js
@@ -26,6 +26,13 @@ Before(function() {
       });
     }
   }).as('Log In');
+
+  cy.intercept('POST', "http://localhost:3001/logout", req => {
+    req.reply({
+      status: 200,
+      logged_out: true,
+    });
+  }).as('Log Out');
 });
 
 Given(`I am on the home page`, () => {
@@ -46,6 +53,15 @@ When(`I visit the login page`, function() {
 
 When(`I reload`, () => {
   cy.reload();
+});
+
+When(`I click the log in page button`, () => {
+  cy.get('[data-testid="log-in-page-button"]').click();
+});
+
+When(`I click the log out button`, () => {
+  cy.get('[data-testid="log-out-button"]').click();
+  cy.setCookie('mock_logged-in', JSON.stringify({ logged_in: false, email: null }));
 });
 
 And(`login status is checked`, () => {
@@ -74,8 +90,8 @@ And('I provide the following:', table => {
   cy.findByLabelText(/password/i).type(password);
 });
 
-And(`I click the log in button`, () => {
-  cy.findByRole('button', { name: /log in/i }).click().wait('@Log In');
+And(`I click the log in form button`, () => {
+  cy.get('[data-testid="log-in-form-button"]').click().wait('@Log In');
 });
 
 And(`I should no longer be on the login page`, () => {
@@ -92,6 +108,27 @@ And(`I should see my first name and last name in the nav bar`, (table) => {
 
 And(`I should remain on the login page`, () => {
   cy.location('pathname').should('eq', '/login');
+});
+
+And(`the log out button should not be visible`, () => {
+  cy.get('[data-testid="log-out-button"]').should('not.exist');
+});
+
+And(`the log in button should not be visible`, () => {
+  cy.get('[data-testid="log-in-button"]').should('not.exist');
+});
+
+Then(`the sign up page button should not be visible`, () => {
+  cy.get('[data-testid="sign-up-page-button"]').should('not.exist');
+});
+
+
+Then(`I should not see my first name and last name in the nav bar`, (table) => {
+  const { firstname, lastname } = table.hashes()[0];
+  cy.findByRole('navigation')
+    .findByText(firstname).should('not.exist');
+  cy.findByRole('navigation')
+    .findByText(lastname).should('not.exist');
 });
 
 Then(`a session should start with the following email:`, (table) => {
@@ -111,4 +148,24 @@ Then(`a session should be active for the same email`, (table) => {
 
 Then(`I should see an error`, () => {
   cy.get('.error').should('be.visible');
+});
+
+Then(`I should be redirected to the login page`, () => {
+  cy.location('pathname').should('eq', '/login');
+});
+
+Then(`I should be redirected to the home page`, () => {
+  cy.location('pathname').should('eq', '/');
+});
+
+Then(`the log in page button should be visible`, () => {
+  cy.get('[data-testid="log-in-page-button"]').should('be.visible');
+});
+
+Then(`the log out button should be visible`, () => {
+  cy.get('[data-testid="log-out-button"]').should('be.visible');
+});
+
+Then(`the sign up page button should be visible`, () => {
+  cy.get('[data-testid="sign-up-page-button"]').should('be.visible');
 });
