d6c1ce59801b6745be32d3da1c292faaa4da5209
rzesch <rzesch@tamu.edu>
2022-10-21 20:19:34 -0500

    Fixed

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_10_21_071837) do
+  
   create_table "allowlist_domains", force: :cascade do |t|
     t.string "email_domain"
     t.string "usertype"
