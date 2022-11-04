185f8392695c67c45d289d68ce7acdb1469fa737
NkemdiAnyiam <NkemdiAnyiam@gmail.com>
2022-11-03 23:09:12 -0500

    Accept automatic schema change

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -37,8 +37,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_21_071837) do
   end
 
   create_table "faqs", force: :cascade do |t|
-    t.string "question"
-    t.text "answer"
+    t.string "question", null: false
+    t.text "answer", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
