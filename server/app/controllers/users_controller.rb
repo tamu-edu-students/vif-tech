40fad35ebc85cb10a18dbccadf2c4246e3bfc5e9
rzesch <rzesch@tamu.edu>
2022-11-03 21:51:32 -0500

    small bugfix

--- server/app/controllers/users_controller.rb
+++ server/app/controllers/users_controller.rb
@@ -112,10 +112,10 @@ class UsersController < ApplicationController
       if params["user"]["usertype"] == "company representative"
         company.users << @user
       end
-      if exact_match != nil
+      if exact_match != nil and company == exact_match.company
         exact_match.users << @user
       end
-      if domain_match != nil
+      if domain_match != nil and company == domain_match.company
         domain_match.users << @user
       end
       logger.debug { resp }
