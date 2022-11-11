834328a6b67108e1cb5cd5fdca295dd07ce65e5d
Niyi Ajobiewe <adeniyi.ajobiewe@gmail.com>
2022-11-11 17:51:56 -0600

    added spec request test

--- server/spec/requests/abouts_spec.rb
+++ server/spec/requests/abouts_spec.rb
@@ -2,6 +2,34 @@ require 'rails_helper'
 
 RSpec.describe "Abouts", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "gets a specific about content" do
+     
+      get abouts_url
+
+      person = {
+        "firstname": "Gary", "lastname": "Cox",
+        "rank": "director", "imgSrc": "www.garycox.com",
+        "social_links": {"facebook": "facebook.com", "twitter": "twitter.com"}
+      }
+      about = About.new person
+      get "/abouts/new"
+      
+    end   
+  end
+
+  describe "GET /show" do
+    it "gives a successful response" do
+
+      person = {
+        "firstname": "Gary", "lastname": "Cox",
+        "rank": "director", "imgSrc": "www.garycox.com",
+        "social_links": {"facebook": "facebook.com", "twitter": "twitter.com"}
+      }
+      about = About.create! person
+      get about_url(about)
+      expect(response).to have_http_status(200)
+
+    end
+
   end
 end
