71f43b5e8dafc8cc00df1e97bdae5c59ab319d24
Niyi Ajobiewe <adeniyi.ajobiewe@gmail.com>
2022-11-10 23:53:14 -0600

    updated schema.rb

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_02_202335) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_04_022906) do
   create_table "abouts", force: :cascade do |t|
     t.string "firstname"
     t.string "lastname"
