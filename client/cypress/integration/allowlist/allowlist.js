ba462019749ef800c8e1fbc198f61cbd26253aee
NkemdiAnyiam <NkemdiAnyiam@gmail.com>
2022-11-12 14:31:55 -0600

    Add tests for adding primary contacts, personal emails, and domains

--- client/cypress/integration/allowlist/allowlist.js
+++ client/cypress/integration/allowlist/allowlist.js
@@ -1,36 +1,48 @@
 /// <reference types="cypress" />
 
 import { Before, Given, When, And, Then } from "cypress-cucumber-preprocessor/steps";
-import { setSession, fetchLoginStatus } from "../utils";
+import { setSession, createCompany, createAllowlistEmail, createAllowlistDomain } from "../utils";
 
 let users = [];
 let companies = [];
 let allowlist_emails = [];
 let allowlist_domains = [];
 
-const createCompany = (() => {
-  let id = 1;
-  const createCompanyWithIdClosure = (name) => ({
-    id: id++,
-    name,
-    description: null,
-    allowlist_emails: [],
-    allowlist_domains: [],
-  });
-  return createCompanyWithIdClosure;
-})();
+const getSubgroupOptions = (subgroup => {
+  let subgroupHeading;
+  let alias;
+
+  if (subgroup === 'primary contact') {
+    subgroupHeading ='Primary Contacts';
+    alias = '@Create Allowlist Email';
+  }
+  if (subgroup === 'personal email') {
+    subgroupHeading = 'Personal Emails';
+    alias = '@Create Allowlist Email';
+  }
+  if (subgroup === 'domain') {
+    subgroupHeading = 'Domains';
+    alias = '@Create Allowlist Domain';
+  }
+
+  return {
+    subgroupHeading,
+    alias,
+  }
+});
 
 Before(function() {
   cy.fixture('users').then(data => { users = data.users; });
   users = [];
-  let companies = [];
-  let allowlist_emails = [];
-  let allowlist_domains = [];
+  companies = [];
+  allowlist_emails = [];
+  allowlist_domains = [];
 
   cy.intercept('GET', "http://localhost:3001/companies", req => {
-    req.reply(200, {
-      companies: []
-    });
+    req.reply(
+      200,
+      { companies }
+    );
   }).as('Fetch Companies');
 
   cy.intercept('POST', "http://localhost:3001/companies", req => {
@@ -48,9 +60,27 @@ Before(function() {
       { company: newCompany }
     );
   }).as('Create Company');
+
+  cy.intercept('POST', "http://localhost:3001/allowlist_emails", req => {
+    const { email } = req.body;
+    const newAllowlistEmail = createAllowlistEmail(email, companies);
+    req.reply(
+      200,
+      { email: newAllowlistEmail }
+    );
+  }).as('Create Allowlist Email');
+
+  cy.intercept('POST', "http://localhost:3001/allowlist_domains", req => {
+    const { domain } = req.body;
+    const newAllowlistDomain = createAllowlistDomain(domain, companies);
+    req.reply(
+      200,
+      { domain: newAllowlistDomain }
+    );
+  }).as('Create Allowlist Domain');
 });
 
-Given(`I visit the companies allow list page`, () => {
+Given(`I visit the companies allowlist page`, () => {
   cy.visit('/profile/company-allowlists');
   cy.wait('@Fetch Companies')
 });
@@ -69,6 +99,20 @@ Given(`I am logged in as an admin`, () => {
   setSession(true, adminEmail, users);
 });
 
+Given(`the following companies are in the allowlist:`, (table) => {
+  const companyNames = Object.values(table.hashes()[0]);
+  companyNames.forEach(companyName => {
+    const company = createCompany(companyName);
+    companies.push(company);
+  });
+  
+  companies.forEach(company => {
+    cy.window().then(win => {
+      win.store.dispatch({ type: 'CREATE_COMPANY', payload: company });
+    });
+  });
+});
+
 When(`I reload`, () => {
   cy.reload();
 });
@@ -78,19 +122,51 @@ When('I click the add new company button', () => {
     .click()
 });
 
-And('I enter the following company name:', (table) => {
-  const { name } = table.hashes()[0];
-  cy.findByLabelText(/company name/i)
-    .type(name); 
+When(/I click the add (.*) button for the following company name:/, (subgroup, table) => {
+  const {companyName} = table.hashes()[0];
+  cy.findByText(new RegExp(`title: ${companyName}`, 'i'))
+    .closest('.allowlist')
+    .findByText(getSubgroupOptions(subgroup).subgroupHeading)
+    .closest('.allowlist__subgroup')
+    .findByRole('button', { name: /add/i })
+    .click();
 });
 
-And('I click the confirm button', () => {
+And('I enter the following company name:', (table) => {
+  const { companyName } = table.hashes()[0];
+  cy.findByLabelText(/company name/i)
+    .type(companyName); 
+});
+
+And('I click the company confirm add button', () => {
   cy.findByRole('button', { name: /confirm/i })
     .click()
     .wait("@Create Company");
 });
 
+And(/I click the (.*) confirm add button/, (subgroup) => {
+  cy.findByRole('button', { name: /confirm/i })
+    .click()
+    .wait(getSubgroupOptions(subgroup).alias);
+});
+
+And(`I enter the following into the modal form:`, (table) => {
+  const { input } = table.hashes()[0];
+  cy.findByRole('textbox')
+    .type(input);
+});
+
 Then(`I should see the following company name in the list:`, (table) => {
-  const { name } = table.hashes()[0];
-  cy.findByTestId("admin-company-allowlists").should('contain', name);
+  const { companyName } = table.hashes()[0];
+  cy.findByTestId("admin-company-allowlists").should('contain', companyName);
+});
+
+Then(/I should see the correct (.*) in the correct company allowlist/, (subgroup, table) => {
+  const { companyName, input } = table.hashes()[0];
+  cy.findByText(new RegExp(`title: ${companyName}`, 'i'))
+    .closest('.allowlist')
+    .findByText(getSubgroupOptions(subgroup).subgroupHeading)
+    .closest('.allowlist__subgroup')
+    .findByRole('list')
+    .should('contain', input);
 });
--- client/cypress/integration/utils.js
+++ client/cypress/integration/utils.js
@@ -1,26 +1,71 @@
 export const setSession = (isLoggedIn, email, users) => {
- cy.intercept('GET', "http://localhost:3001/logged_in", req => {
-  if (isLoggedIn) {
-    const matchingUser = users.find(({ email: targetEmail }) => targetEmail === email);
-    expect(matchingUser).to.not.equal(null);
-    req.reply({
-      status: 200,
-      logged_in: true,
-      user: matchingUser,
-    },
-    {
-      'Set-Cookie': `mock_logged-in=${JSON.stringify({ logged_in: true, email })}`
-    });
-  }
-  else {
-    req.reply({
-      status: 200,
-      logged_in: false,
-      user: null
-    },
-    {
-      'Set-Cookie': `mock_logged-in=${JSON.stringify({ logged_in: false, email: null })}`
-    });
-  }
- }).as('Logged In');
-}
+  cy.intercept('GET', "http://localhost:3001/logged_in", req => {
+   if (isLoggedIn) {
+     const matchingUser = users.find(({ email: targetEmail }) => targetEmail === email);
+     expect(matchingUser).to.not.equal(null);
+     req.reply({
+       status: 200,
+       logged_in: true,
+       user: matchingUser,
+     },
+     {
+       'Set-Cookie': `mock_logged-in=${JSON.stringify({ logged_in: true, email })}`
+     });
+   }
+   else {
+     req.reply({
+       status: 200,
+       logged_in: false,
+       user: null
+     },
+     {
+       'Set-Cookie': `mock_logged-in=${JSON.stringify({ logged_in: false, email: null })}`
+     });
+   }
+  }).as('Logged In');
+ }
+
+export const createCompany = (() => {
+  let id = 1;
+  const createCompanyWithIdClosure = (name) => ({
+    id: id++,
+    name,
+    description: null,
+    allowlist_emails: [],
+    allowlist_domains: [],
+  });
+  return createCompanyWithIdClosure;
+})();
+
+export const createAllowlistEmail = (() => {
+  let id = 1;
+  const createAllowlistEmailWithIdClosure = ({email, company_id, usertype, isPrimaryContact}, companies) => {
+    const newAllowlistEmail = {
+      id: id++,
+      email,
+      ...(company_id && {company_id: company_id}),
+      usertype,
+      isPrimaryContact: isPrimaryContact === 1 ? true : false,
+    }
+    const matchingCompany = companies.find(company => company.id === newAllowlistEmail.company_id);
+    if (matchingCompany) { matchingCompany.allowlist_emails.push(newAllowlistEmail); }
+    return newAllowlistEmail;
+  };
+  return createAllowlistEmailWithIdClosure;
+})();
+
+export const createAllowlistDomain = (() => {
+  let id = 1;
+  const createAllowlistDomainWithIdClosure = ({email_domain, company_id, usertype}, companies) => {
+    const newAllowlistDomain = {
+      id: id++,
+      email_domain,
+      ...(company_id && {company_id: company_id}),
+      usertype,
+    }
+    const matchingCompany = companies.find(company => company.id === newAllowlistDomain.company_id);
+    if (matchingCompany) { matchingCompany.allowlist_domains.push(newAllowlistDomain); }
+    return newAllowlistDomain;
+  };
+  return createAllowlistDomainWithIdClosure;
+})();
