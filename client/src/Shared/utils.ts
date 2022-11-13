3969622ff14b365bdbb9fa1dae44ee6f65bcd745
NkemdiAnyiam <NkemdiAnyiam@gmail.com>
2022-11-12 21:33:35 -0600

    Force fractional wait time before checking login to allow time for Cypress to set store on window

--- client/src/Shared/utils.ts
+++ client/src/Shared/utils.ts
@@ -1,1 +1,2 @@
 export const wait = (milliseconds: number) => new Promise(resolve => setTimeout(resolve, milliseconds));
+export const waitThen = (milliseconds: number, func: Function) => wait(milliseconds).then(() => func());
