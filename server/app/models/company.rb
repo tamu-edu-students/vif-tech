16abd4069874cbd7331c8fff3ae881520ef5be98
ganan-tamu <ganan@tamu.edu>
2022-11-16 00:25:01 -0600

    added showForStudents() and its rspec test

--- server/app/models/company.rb
+++ server/app/models/company.rb
@@ -4,7 +4,7 @@ class Company < ApplicationRecord
     has_many :allowlist_emails
     validates :name, presence: true, uniqueness: true
 
-    def showForStudent(company_id)
+    def self.showForStudent(company_id)
         Company.select('id, name, description, logo_src').find_by_id(company_id) # nil or record obj
     end
 
--- server/spec/models/company_spec.rb
+++ server/spec/models/company_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Company, type: :model do
   it "displays only some information of a company to students" do
     company = Company.create(:name => 'disney', :id => 2, :description => 'Ewoooo...!/\~@', :logo_src => 'https://thumbs.dreamstime.com/b/disney-company-logo-sign-grey-plastic-letters-white-wall-moscow-russia-march-86290724.jpg')
     # p company
-    c = company.showForStudent(company.id) 
+    c = Company.showForStudent(company.id) 
     # p c 
     assert_raises(ActiveModel::MissingAttributeError) {c.created_at}
     assert_raises(ActiveModel::MissingAttributeError) {c.updated_at}
