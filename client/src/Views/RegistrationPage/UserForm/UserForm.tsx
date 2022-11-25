ee699a917566c5b3cb96d15665b1231a92f28054
NkemdiAnyiam <NkemdiAnyiam@gmail.com>
2022-11-25 13:09:35 -0600

    Fix erroneous modifcation to mapDispatchToProps

--- client/src/Views/RegistrationPage/UserForm/UserForm.tsx
+++ client/src/Views/RegistrationPage/UserForm/UserForm.tsx
@@ -24,7 +24,7 @@ const mapDispatchToProps = (dispatch: any, ownProps: any) => {
     changeFieldValue : function(field: any, value: any) {
       dispatch(change(formName, field, value));
     },
-    fetchCompanies,
+    fetchCompanies: function() { dispatch(fetchCompanies()) },
   }
 }
 const connector = connect(mapStateToProps, mapDispatchToProps);
