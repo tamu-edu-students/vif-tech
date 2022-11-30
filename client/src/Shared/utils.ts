dbdf14c22994256e20176cbe7aacf475033ea732
NkemdiAnyiam <NkemdiAnyiam@gmail.com>
2022-11-30 00:43:21 -0600

    Start setting up structure for generating time column

--- client/src/Shared/utils.ts
+++ client/src/Shared/utils.ts
@@ -1,2 +1,27 @@
 export const wait = (milliseconds: number) => new Promise(resolve => setTimeout(resolve, milliseconds));
 export const waitThen = (milliseconds: number, func: Function) => wait(milliseconds).then(() => func());
+export const msToTimeString = (dateMilliseconds: number) => {
+  return Intl.DateTimeFormat("en-CA", {
+    hour: "numeric",
+    minute: "numeric",
+    timeZone: 'CST',
+    timeZoneName: 'short'
+  })
+  .format(new Date(dateMilliseconds))
+  .replaceAll(/(\s\w{3})|(,)|(\s\w\.m\.)/gi, '');
+};
+export const msToDateTimeString = (dateMilliseconds: number) => {
+  return Intl.DateTimeFormat("en-CA", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    hour12: false,
+    timeZone: 'CST',
+    timeZoneName: 'short'
+  })
+  .format(dateMilliseconds)
+  .replaceAll(/(\s\w{3})|(,)/gi, '')
+};
--- client/src/Views/ProfilePage/VolunteerTimesheet/VolunteerTimesheet.tsx
+++ client/src/Views/ProfilePage/VolunteerTimesheet/VolunteerTimesheet.tsx
@@ -6,6 +6,7 @@ import { } from 'Store/actions/types';
 import { } from 'Store/actions';
 
 import { Usertype } from 'Shared/enums';
+import { msToTimeString } from 'Shared/utils';
 
 
 interface OwnProps {
@@ -14,17 +15,43 @@ interface OwnProps {
 interface OwnState {
 }
 
+type TimeOption = {
+  start: string;
+  end: string;
+}
+
 const mapStateToProps = (state: IRootState, ownProps: any) => {
   return {
-
-  }
-}
+    timeSlots: [
+      {start: '2023-02-16 10:00:00', end: '2023-02-16 10:20:00'},
+      {start: '2023-02-16 10:25:00', end: '2023-02-16 10:45:00'},
+      {start: '2023-02-16 10:50:00', end: '2023-02-16 11:10:00'},
+    ],
+  };
+};
 const mapDispatchToProps = { };
 const connector = connect(mapStateToProps, mapDispatchToProps);
 
 type Props = ConnectedProps<typeof connector> & OwnProps;
 
 class VolunteerTimesheet extends React.Component<Props, OwnState> {
+  private _renderTimeOptions(): JSX.Element[] {
+    return this.props.timeSlots.map(({start, end}: TimeOption) => {
+      const startTimeShort = msToTimeString(Date.parse(start));
+      const endTimeShort = msToTimeString(Date.parse(end));
+      return (
+        <div key={startTimeShort} className="table__row">
+          <div className="table__cell table__cell--time">
+            {`${startTimeShort}—${endTimeShort}`}
+          </div>
+          <div className="table__cell table__cell--name">Mark Child</div>
+          <div className="table__cell table__cell--portfolio">meganhecklinger.myportfolio.com</div>
+          <div className="table__cell table__cell--resume">meganhecklinger.myportfolio.com</div>
+        </div>
+      )
+    });
+  }
+
   public render(): React.ReactElement<Props> {
     return (
       <div className="Volunteer-Timesheet">
@@ -39,24 +66,7 @@ class VolunteerTimesheet extends React.Component<Props, OwnState> {
               <div className="table__cell table__cell--header">Resume</div>
             </div>
 
-            <div className="table__row">
-              <div className="table__cell table__cell--time">Time</div>
-              <div className="table__cell table__cell--name">Name</div>
-              <div className="table__cell table__cell--portfolio">Portfolio</div>
-              <div className="table__cell table__cell--resume">Resume</div>
-            </div>
-            <div className="table__row">
-              <div className="table__cell table__cell--time">Time</div>
-              <div className="table__cell table__cell--name">Name</div>
-              <div className="table__cell table__cell--portfolio">Portfolio</div>
-              <div className="table__cell table__cell--resume">Resume</div>
-            </div>
-            <div className="table__row">
-              <div className="table__cell table__cell--time">Time</div>
-              <div className="table__cell table__cell--name">Name</div>
-              <div className="table__cell table__cell--portfolio">Portfolio</div>
-              <div className="table__cell table__cell--resume">Resume</div>
-            </div>
+            {this._renderTimeOptions()}
 
           </div>
         </div>
