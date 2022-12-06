ad44f15c00b9d8faf575bcd74636c4f94f188b69
NkemdiAnyiam <NkemdiAnyiam@gmail.com>
2022-12-05 19:38:03 -0600

    Improve coloring of timesheets and timetables

--- client/src/Views/ProfilePage/MeetingAssignmentSheet/VolunteerTimetable/AssignmentRow/AssigmentRow.tsx
+++ client/src/Views/ProfilePage/MeetingAssignmentSheet/VolunteerTimetable/AssignmentRow/AssigmentRow.tsx
@@ -9,8 +9,16 @@ import { msToTimeString } from 'Shared/utils';
 // import Event from 'Shared/entityClasses/Event';
 import Meeting from 'Shared/entityClasses/Meeting';
 import StudentSelectForm from './StudentSelectForm/StudentSelectForm';
+import User from 'Shared/entityClasses/User';
 
 import { OptionsContext } from '../../OptionsContext';
+
+enum Status {
+  NO_CHANGE,
+  DELETING,
+  ADDING,
+  SWAPPING,
+}
 
 
 interface OwnProps {
@@ -21,19 +29,17 @@ interface OwnProps {
 }
 
 interface OwnState {
-  isChanged: boolean;
+  status: Status;
 }
-
-// type TimeOption = {
-//   start_time: string;
-//   end_time: string;
-// }
 
 const mapStateToProps = (state: IRootState, ownProps: any) => {
   const meeting: Meeting = ownProps.meeting;
+  const initialInvitee: User | null = meeting?.findInvitee(state.userData.users) ?? null;
+  const hadAssignment: boolean = initialInvitee ? true : false;
   return {
     owner_id: meeting.owner_id,
-    initialInvitee: meeting?.findInvitee(state.userData.users) ?? null,
+    initialInvitee,
+    hadAssignment,
     keyVal: `${ownProps.event_id} ${meeting.id} ${ownProps.owner_id} ${ownProps.start_time} ${ownProps.end_time}`,
   };
 };
@@ -43,7 +49,7 @@ const connector = connect(mapStateToProps, mapDispatchToProps);
 type Props = ConnectedProps<typeof connector> & OwnProps;
 
 class AssignmentRow extends React.Component<Props, OwnState> {
-  state = { isChanged: false };
+  state = { status: Status.NO_CHANGE };
   static contextType = OptionsContext;
   context!: React.ContextType<typeof OptionsContext>;
 
@@ -57,20 +63,25 @@ class AssignmentRow extends React.Component<Props, OwnState> {
     if (initialInvitee) {
       if (inviteeId === -1){
         reaction = () => this.props.updateMeeting(this.props.meeting.id, -1);
+        this.setState({ status: Status.DELETING });
       }
       else if (initialInvitee.id === inviteeId) {
         reaction = () => Promise.resolve();
+        this.setState({ status: Status.NO_CHANGE });
       }
       else {
         reaction = () => this.props.updateMeeting(this.props.meeting.id, inviteeId);
+        this.setState({ status: Status.SWAPPING });
       }
     }
     else {
       if (inviteeId === -1) {
         reaction = () => Promise.resolve();
+        this.setState({ status: Status.NO_CHANGE });
       }
       else {
         reaction = () => this.props.updateMeeting(this.props.meeting.id, inviteeId);
+        this.setState({ status: Status.ADDING });
       }
     }
 
@@ -81,9 +92,12 @@ class AssignmentRow extends React.Component<Props, OwnState> {
     const {
       start_time,
       end_time,
+      hadAssignment,
       keyVal: key,
       initialInvitee,
     } = this.props;
+
+    const { status } = this.state;
     
     const startTimeShort = msToTimeString(Date.parse(start_time), 'CST');
     const endTimeShort = msToTimeString(Date.parse(end_time), 'CST');
@@ -93,10 +107,15 @@ class AssignmentRow extends React.Component<Props, OwnState> {
         <div className="table__cell table__cell--time">
           <div
             className={`table__time-block ${
-              ''
-              // isAvailable
-              // ? (this.state.isChanged ? 'table__time-block--deleting' : 'table__time-block--available')
-              // : (this.state.isChanged ? 'table__time-block--adding' : '')
+              hadAssignment
+              ? (
+                  (status === Status.DELETING ? 'table__time-block--deleting' : '')
+                  || (status === Status.SWAPPING ? 'table__time-block--swapping' : '')
+                  || (status === Status.NO_CHANGE ? 'table__time-block--filled' : '')
+                )
+              : (
+                (status === Status.ADDING ? 'table__time-block--adding' : '')
+              )
             }`}
           >
               {`${startTimeShort}—${endTimeShort}`}
